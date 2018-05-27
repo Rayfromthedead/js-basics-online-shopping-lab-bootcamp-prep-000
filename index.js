0cd173fc3d1b0cbd15f87cad4d6d9a463751f6f4
ray.rodz.88@gmail.com Rodriguez <ray.rodz.88@gmail.com>
2018-05-27 14:15:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var cart;
 var cart = [];
 
 function getCart() {
@@ -10,18 +11,29 @@ function setCart(c) {
 }
 
 function addToCart(item) {
+  var price = Math.random(0,100);
+  function getRandomInt(min, max) {
+  min = Math.ceil(0);
+  max = Math.floor(1);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
  // write your code here
 }
 
 function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
+  if (cart.length === 0) {
+    return ("Your shopping cart is empty.");
+    
+  }
+  var inYourCart= [];
+  var i = 0;
+  while (i < cart.length){
+    var item = Object.keys(cart[i])[0];
+    var price = cart[i][item];
+    inYourCart.push(item + "at $" + price);
+    i++}
+    return ("In your cart, you have ${inYourCart.join}(", ")}."
+    );
   // write your code here
 }
 
